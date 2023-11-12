c2d9af24745b8d0a6eef5422fdd0714e79ad135f
Nikodem Nikiel <76944706+VeliverX@users.noreply.github.com>
2023-11-13 00:09:26 +0100

    dodanie geolokalizacji poczatkowej

--- MyApp/www/js/index.js
+++ MyApp/www/js/index.js
@@ -1,29 +1,42 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-
-// Wait for the deviceready event before using any of Cordova's device APIs.
-// See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
 document.addEventListener('deviceready', onDeviceReady, false);
 
-function onDeviceReady() {
-    // Cordova is now initialized. Have fun!
+// Pobierz poprzednią pozycję użytkownika
 
-    console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
-    document.getElementById('deviceready').classList.add('ready');
+
+function onDeviceReady() {
+
+    navigator.geolocation.getCurrentPosition(position => {
+        console.log('Current Location:', position.coords.latitude, position.coords.longitude);
+        
+        const geo_latitude = position.coords.latitude;
+        const geo_longitude = position.coords.longitude;
+
+        const currentLocation = {
+            geo_latitude: geo_latitude,
+            geo_longitude: geo_longitude,
+            name: 'Moja lokalizacja',
+            description: 'Bieżąca lokalizacja użytkownika'
+        };
+
+    const map = L.map('map').setView([geo_latitude, geo_longitude], 10);
+    const control = L.Routing.control({
+      waypoints: [],
+      routeWhileDragging: true,
+      createMarker: function (i, wp, nWps) {
+        const marker = L.marker(wp.latLng)
+          .bindPopup(`<b>${wp.name}</b><br>${wp.description || ''}`)
+          .on('mouseover', function () {
+            this.openPopup();
+          })
+          .on('mouseout', function () {
+            this.closePopup();
+          });
+        return marker;
+      }
+    }).addTo(map);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '© OpenStreetMap contributors'
+      }).addTo(map);
+    });
 }
