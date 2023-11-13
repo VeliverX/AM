bf8350a80ef4abbe968b089bbf4f06a45e885d68
Nikodem Nikiel <76944706+VeliverX@users.noreply.github.com>
2023-11-13 12:52:49 +0100

    koniec zmian

--- MyApp/www/js/index.js
+++ MyApp/www/js/index.js
@@ -1,42 +1,120 @@
 document.addEventListener('deviceready', onDeviceReady, false);
 
-// Pobierz poprzednią pozycję użytkownika
-
+let Trasy;
 
 function onDeviceReady() {
+  
+  const LocalStorage_Trasy = JSON.parse(localStorage.getItem('Trasy'));
+  const map = L.map('map').setView([50, 20], 7);
+  const control = L.Routing.control({
+    waypoints: [],
+    routeWhileDragging: true, // mozliwosc zmiany trasy
+    geocoder: L.Control.Geocoder.nominatim(), //leaflet-control-geocoder
+    createMarker: function (i, point, nWps) {
+      const marker = L.marker(point.latLng)
+        .bindPopup(`<b>${point.name}</b><br>${point.description || ''}`)
+        .on('click', function () { //touchstart w przypadku urządzenia mobilnego
+          this.openPopup();
+        })
+        .on('mouseout', function () {
+          this.closePopup();
+        });
+      return marker;
+    }
+  }).addTo(map);
 
-    navigator.geolocation.getCurrentPosition(position => {
-        console.log('Current Location:', position.coords.latitude, position.coords.longitude);
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '© OpenStreetMap contributors'
+  }).addTo(map);
+
+  const select = document.getElementById('ListaTras');
+  const openInMapsButton = document.getElementById('GoogleMapsButton');
+
+
+  navigator.geolocation.getCurrentPosition(position => {
         
         const geo_latitude = position.coords.latitude;
         const geo_longitude = position.coords.longitude;
 
-        const currentLocation = {
+        const centerMapButton = document.getElementById('centerMapButton');
+        centerMapButton.addEventListener('click', () => {
+          map.setView([position.coords.latitude, position.coords.longitude], 15);
+        });
+
+        const Geo_Localization = {
             geo_latitude: geo_latitude,
             geo_longitude: geo_longitude,
-            name: 'Moja lokalizacja',
-            description: 'Bieżąca lokalizacja użytkownika'
+            name: 'Geolokacja',
+            description: 'Aktualna geolokacja'
         };
 
-    const map = L.map('map').setView([geo_latitude, geo_longitude], 10);
-    const control = L.Routing.control({
-      waypoints: [],
-      routeWhileDragging: true,
-      createMarker: function (i, wp, nWps) {
-        const marker = L.marker(wp.latLng)
-          .bindPopup(`<b>${wp.name}</b><br>${wp.description || ''}`)
-          .on('mouseover', function () {
-            this.openPopup();
-          })
-          .on('mouseout', function () {
-            this.closePopup();
-          });
-        return marker;
-      }
-    }).addTo(map);
+    if (LocalStorage_Trasy) {
+      Trasy = LocalStorage_Trasy;
+      initializeRoutes(Trasy, Geo_Localization, select, control);
+    } else {
+      // Pobierz trasy z serwera
+      fetch('http://localhost:8080/Trasy')
+        .then(response => response.json())
+        .then(data => {
+          Trasy = data;
+          console.log('Routes from server:', Trasy);
 
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '© OpenStreetMap contributors'
-      }).addTo(map);
+          // Zapisz trasy w lokalnym magazynie danych
+          localStorage.setItem('Trasa', JSON.stringify(Trasy));
+
+          initializeRoutes(Trasy, Geo_Localization, select, control);
+        })
+        .catch(error => {
+          console.error('Błąd podczas pobierania tras:', error);
+        });
+    }
+  });
+
+  function initializeRoutes(Trasa, Geo_Localization, select, control) {
+    // Dodaj bieżącą lokalizację jako pierwszy punkt do każdej trasy
+    Trasa.forEach(route => {
+      route.waypoints.unshift(Geo_Localization);
+
+      // Aktualizuj listę tras w selekcie
+      const option = document.createElement('option');
+      option.value = route.id;
+      option.text = route.name;
+      select.add(option);
     });
+
+    // Inicjalizuj trasę na podstawie pierwszej trasy
+    const selectedRouteId = select.value;
+    updateRoute(selectedRouteId, Trasa, control);
+  }
+
+  select.addEventListener('change', () => {
+    const selectedRouteId = select.value;
+    updateRoute(selectedRouteId, Trasy, control);
+  });
+
+  openInMapsButton.addEventListener('click', () => {
+    const selectedRouteId = select.value;
+    const selectedRoute = Trasy.find(route => route.id == selectedRouteId);
+
+    if (selectedRoute) {
+      const waypoints = selectedRoute.waypoints.map(waypoint => `${waypoint.geo_latitude},${waypoint.geo_longitude}`);
+      const url = `https://www.google.com/maps/dir/${waypoints.join('/')}`;
+      window.open(url, '_system');
+    }
+  });
+
+  function updateRoute(selectedRouteId, Trasa, control) {
+    const route = Trasa.find(route => route.id == selectedRouteId);
+
+    if (route) {
+      const waypoints = route.waypoints.map(waypoint => ({
+        latLng: L.latLng(waypoint.geo_latitude, waypoint.geo_longitude),
+        name: waypoint.name,
+        description: waypoint.description || ''
+      }));
+
+      control.setWaypoints(waypoints);
+    }
+  }
+  
 }
--- MyApp/www/js/routes_server.js
+++ MyApp/www/js/routes_server.js
@@ -8,45 +8,44 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
 });
-const routes = [
+const Trasy = [
     {
         id: 1,
         name: 'Trasa Kety, Rybnik, Raciórz, Bystrzyca Kłodzka',
         waypoints: [
             
-          { id: 1, name: 'Kety, Rynek', lat: 49.88282586208495, lng: 19.220571140904532,description: 'miasto w województwie małopolskim, w powiecie oświęcimskim, siedziba gminy miejsko-wiejskiej Kęty. Kęty to miasto o charakterze handlowo-przemysłowym, położone na Podgórzu Wilamowickim, w dolinie rzeki Soły.' },
-          { id: 2, name: 'Rybnik', lat: 50.0773601, lng: 18.5484949,description: 'miasto na prawach powiatu w południowej Polsce, w województwie śląskim, na Płaskowyżu Rybnickim, nad rzekami Rudą i Nacyną' },
-          { id: 3, name: 'Racibórz', lat: 50.1001870, lng: 18.1992321,description: 'miasto w Polsce, w województwie śląskim; siedziba władz powiatu raciborskiego. Racibórz jest obok Opola jedną z historycznych stolic Górnego Śląska, gdzie rezydowali książęta opolsko-raciborscy.' },
-          { id: 4, name: 'Bystrzyca Kłodzka', lat: 50.29927789404893, lng: 16.64180664252121,description: ' miasto w południowo-zachodniej Polsce, w województwie dolnośląskim, w powiecie kłodzkim, siedziba gminy miejsko-wiejskiej Bystrzyca Kłodzka. Historycznie było położone w hrabstwie kłodzkim.' , }
+          { id: 1, name: 'Kety, Rynek', geo_latitude: 49.88282586208495, geo_longitude: 19.220571140904532,description: 'miasto w województwie małopolskim, w powiecie oświęcimskim, siedziba gminy miejsko-wiejskiej Kęty. Kęty to miasto o charakterze handlowo-przemysłowym, położone na Podgórzu Wilamowickim, w dolinie rzeki Soły.' },
+          { id: 2, name: 'Rybnik', geo_latitude: 50.0773601, geo_longitude: 18.5484949,description: 'miasto na prawach powiatu w południowej Polsce, w województwie śląskim, na Płaskowyżu Rybnickim, nad rzekami Rudą i Nacyną' },
+          { id: 3, name: 'Racibórz', geo_latitude: 50.1001870, geo_longitude: 18.1992321,description: 'miasto w Polsce, w województwie śląskim; siedziba władz powiatu raciborskiego. Racibórz jest obok Opola jedną z historycznych stolic Górnego Śląska, gdzie rezydowali książęta opolsko-raciborscy.' },
+          { id: 4, name: 'Bystrzyca Kłodzka', geo_latitude: 50.29927789404893, geo_longitude: 16.64180664252121,description: ' miasto w południowo-zachodniej Polsce, w województwie dolnośląskim, w powiecie kłodzkim, siedziba gminy miejsko-wiejskiej Bystrzyca Kłodzka. Historycznie było położone w hrabstwie kłodzkim.' , }
         ],
       },
       {
         id: 2,
         name: 'Trasa Kęty, Chrobacza Łąka',
         waypoints: [
-          { id: 5, name: 'Dworzec PKP, Kęty Podlesie',lat: 49.87816959538084,  lng: 19.196828813560234, description: 'Opis miejsca' },
-          { id: 6, name: 'Rondo, Kozy', lat: 49.84464145422655, lng: 19.142363840338472,description: 'Opis miejsca' },
-          { id: 7, name: 'Kamieniołom, Kozy', lat: 49.82931467650209, lng: 19.16466673510428,description: 'Opis miejsca' },
-          { id: 8, name: 'Chrobacza Łąka', lat: 49.822022602140734,  lng: 19.16606830189463,description: 'Opis miejsca' },
+          { id: 5, name: 'Dworzec PKP, Kęty Podlesie',geo_latitude: 49.87816959538084,  geo_longitude: 19.196828813560234, description: 'Dworzec kolejnowy, Kęty podlesie. Bilet online lub u konduktora' },
+          { id: 6, name: 'Rondo, Kozy', geo_latitude: 49.84464145422655, geo_longitude: 19.142363840338472,description: 'Rondo kapitana Krzysztofa Grabowskiego' },
+          { id: 7, name: 'Kamieniołom, Kozy', geo_latitude: 49.82931467650209, geo_longitude: 19.16466673510428,description: ' nieczynny kamieniołom piaskowców lgockich położony na zboczu Chrobaczej Łąki, w obrębie grupy Magurki Wilkowickiej w Kozach, w powiecie bielskim, w województwie śląskim. Działał w latach 1910–1992' },
+          { id: 8, name: 'Chrobacza Łąka', geo_latitude: 49.822022602140734,  geo_longitude: 19.16606830189463,description: 'szczyt w Grupie Magurki Wilkowickiej w Beskidzie Małym. Znajduje się w głównym grzbiecie tej grupy, między przełęczą Przegibek a Bujakowskim Groniem. Z północno-zachodnich stoków spływają źródłowe cieki potoku Kozówka i na stoku tym znajduje się duży Kamieniołom w Kozach' },
         ],
-      },
-      
+      }, 
 ];
 
 // Obsługa zapytania GET dla listy tras
-app.get('/routes', (req, res) => {
-  res.json(routes);
+app.get('/Trasy', (req, res) => {
+  res.json(Trasy);
 });
 
 
-app.get('/routes/:id', (req, res) => {
-  const routeId = parseInt(req.params.id);
-  const route = routes.find(r => r.id === routeId);
+app.get('/Trasy/:id', (req, res) => {
+  const Id_Trasy = parseInt(req.params.id);
+  const Trasa = Trasy.find(r => r.id === Id_Trasy);
 
-  if (route) {
-    res.json(route);
+  if (Trasa) {
+    res.json(Trasa);
   } else {
-    res.status(404).json({ error: 'Trasa nie znaleziona' });
+    res.status(404).json({ error: 'Trasy nie znaleziona' });
   }
 });
 
